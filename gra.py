ebd756a732c85121ebcd4f2e0118a23dc583fa3c
Urgith <patrykolejniczak7@gmail.com>
2022-04-21 20:46:04 +0200

    PEP8: class Przeciwnik, class Wieza

--- gra.py
+++ gra.py
@@ -298,136 +298,180 @@ class Pocisk:
 
 
 class Przeciwnik:
-  def __init__(self, gra):
-    self.rodzaj=    waves[gra.runda][gra.numer_przeciwnika]
-    self.rozmiar=   enemies[self.rodzaj][5]
-    self.x, self.y= 5, 5
-    self.ids=       []
-    self.obiekt=    pygame.Rect((self.x, self.y, self.rozmiar, self.rozmiar))
-    self.pole=      mapa[0][0]
-    self.startowe_zdrowie= int(enemies[self.rodzaj][0]*(1.1**gra.runda))
-    self.zdrowie=          int(enemies[self.rodzaj][0]*(1.1**gra.runda))
-    self.predkosc=         enemies[self.rodzaj][1]
-    self.atak=             enemies[self.rodzaj][2]
-    self.punkty=           enemies[self.rodzaj][3]
-    self.monety=           enemies[self.rodzaj][4]
 
-  def ruch(self):
-    pole= mapa[int((self.y-5)/TILESIZE)][int((self.x-5)/TILESIZE)]
-    if   (self.pole==6 or self.pole==60) and (self.pole!=pole): pole= mapa[int((self.y-5)/TILESIZE)][int((self.x+20-self.predkosc)/TILESIZE)]
-    elif (self.pole==7 or self.pole==70) and (self.pole!=pole): pole= mapa[int((self.y+20-self.predkosc)/TILESIZE)][int((self.x+20-self.predkosc)/TILESIZE)]
+    def __init__(self, gra):
+        self.rodzaj = waves[gra.runda][gra.numer_przeciwnika]
 
-    if   pole==0 or pole==10: pole= self.pole
-    if   pole==5 or pole==50: self.x+= self.predkosc
-    elif pole==6 or pole==60: self.x-= self.predkosc
-    elif pole==7 or pole==70: self.y-= self.predkosc
-    elif pole==8 or pole==80: self.y+= self.predkosc
-    self.pole= pole
+        self.rozmiar = enemies[self.rodzaj][5]
+        self.x, self.y = 5, 5
+        self.obiekt = pygame.Rect((self.x, self.y, self.rozmiar, self.rozmiar))
 
-    self.obiekt.x, self.obiekt.y= self.x, self.y
+        self.pole = mapa[0][0]
+        self.startowe_zdrowie = int(enemies[self.rodzaj][0] * (1.1**gra.runda))
+        self.zdrowie = int(enemies[self.rodzaj][0] * (1.1**gra.runda))
+        self.predkosc = enemies[self.rodzaj][1]
+        self.atak = enemies[self.rodzaj][2]
+        self.punkty = enemies[self.rodzaj][3]
+        self.monety = enemies[self.rodzaj][4]
+
+        self.ids = []
+
+    def ruch(self):
+        pole = mapa[int((self.y - 5) / TILESIZE)][int((self.x - 5) / TILESIZE)]
+
+        if (self.pole == 6 or self.pole == 60) and self.pole != pole:
+            pole = mapa[int((self.y - 5) / TILESIZE)][int((self.x + 20 - self.predkosc) / TILESIZE)]
+        elif (self.pole == 7 or self.pole == 70) and self.pole != pole:
+            pole = mapa[int((self.y + 20 - self.predkosc) / TILESIZE)][int((self.x + 20 - self.predkosc) / TILESIZE)]
+
+        if pole == 0 or pole == 10:
+            pole = self.pole
+
+        if pole == 5 or pole == 50:
+            self.x += self.predkosc
+        elif pole == 6 or pole == 60:
+            self.x -= self.predkosc
+        elif pole == 7 or pole == 70:
+            self.y -= self.predkosc
+        elif pole == 8 or pole == 80:
+            self.y += self.predkosc
+
+        self.obiekt.x, self.obiekt.y = self.x, self.y
+        self.pole = pole
 
 class Wieza:
-  def __init__(self, gra):
-    self.obiekt=   pygame.Rect(gra.pozycja_myszy[0]-10, gra.pozycja_myszy[1]-10, 20, 20)
-    self.pole=     gra.pozycja_myszy
-    self.rodzaj=   gra.rodzaj_wybranej_wiezy
-    self.licznik=  0
-    self.predkosc= 1
-    self.poziom_atak, self.poziom_zasieg, self.poziom_reszta, self.poziom=  0, 0, 0, 0
+    def __init__(self, gra):
+        self.obiekt = pygame.Rect(gra.pozycja_myszy[0] - 10, gra.pozycja_myszy[1] - 10, 20, 20)
+        self.pole = gra.pozycja_myszy
+        self.rodzaj = gra.rodzaj_wybranej_wiezy
+        self.licznik = 0
+        self.predkosc = 1
+        self.poziom_atak, self.poziom_zasieg, self.poziom_reszta, self.poziom = (0, 0, 0, 0)
 
-    self.typ, self.kolor, self.koszt, self.obrazenia, self.zasieg, self.przeladowanie, self.dlugosc_zycia, self.rozmiar_pocisku= wieze[self.rodzaj-1]
-    self.cena_calkowita= self.koszt
-    if self.rodzaj==1:    self.koszt_atak=4; self.koszt_zasieg=1; self.koszt_reszta=2
-    elif self.rodzaj==2: self.koszt_atak=10; self.koszt_zasieg=2; self.koszt_reszta=20; self.przebicie=  1
-    elif self.rodzaj==3: self.koszt_atak=40; self.koszt_zasieg=4; self.koszt_reszta=30; self.ilu_na_raz= 3; self.elektryzacja= 0
+        (self.typ, self.kolor, self.koszt, self.obrazenia, self.zasieg,
+            self.przeladowanie,self.dlugosc_zycia, self.rozmiar_pocisku) = wieze[self.rodzaj-1]
 
-  def strzal(self, gra):
-    if self.licznik+self.przeladowanie<=gra.licznik:
-      self.licznik= gra.licznik
-      gra.lista_pociskow.append(Pocisk(self, gra))
+        self.cena_calkowita = self.koszt
+        if self.rodzaj == 1: 
+            self.koszt_atak = 4
+            self.koszt_zasieg = 1
+            self.koszt_reszta = 2
 
-  def polepszenie(self, gra):
-    gra.wybrano_wieze= True
-    if self.rodzaj==1:
-      if gra.polepszenie%4==0:
-        if gra.pieniadze>=4 and self.poziom_atak<=4:
-          self.cena_calkowita+= 4
-          gra.pieniadze-=       4
-          self.obrazenia+=      5
-          self.poziom_atak+=    1
-      elif gra.polepszenie%4==1:
-        if gra.pieniadze>=1 and self.poziom_zasieg<=4:
-          self.cena_calkowita+= 1
-          gra.pieniadze-=       1
-          self.dlugosc_zycia+=  30
-          self.zasieg+=         15
-          self.poziom_zasieg+=  1
-      elif gra.polepszenie%4==2:
-        if gra.pieniadze>=2 and self.poziom_reszta<=4:
-          self.cena_calkowita+= 2
-          gra.pieniadze-=       2
-          self.przeladowanie-=  1
-          self.predkosc+=       0.4
-          self.poziom_reszta+=  1
-      elif gra.polepszenie%4==3:
-        gra.wybrano_wieze= False
-        gra.pieniadze+=    self.cena_calkowita
-        gra.lista_wiez.pop(gra.wybrana_wieza)
-    elif self.rodzaj==2:
-      if gra.polepszenie%4==0:
-        if gra.pieniadze>=10 and self.poziom_atak<=4:
-          self.cena_calkowita+= 10
-          gra.pieniadze-=       10
-          self.obrazenia+=      20
-          self.poziom_atak+=    1
-      elif gra.polepszenie%4==1:
-        if gra.pieniadze>=2 and self.poziom_zasieg<=4:
-          self.cena_calkowita+= 2
-          gra.pieniadze-=       2
-          self.dlugosc_zycia+=  15
-          self.zasieg+=         30
-          self.poziom_zasieg+=  1
-      elif gra.polepszenie%4==2:
-        if gra.pieniadze>=20 and self.poziom_reszta<=4:
-          self.cena_calkowita+= 20
-          gra.pieniadze-=       20
-          self.przeladowanie-=  1
-          self.przebicie+=      1
-          self.poziom_reszta+=  1
-      elif gra.polepszenie%4==3:
-        gra.wybrano_wieze= False
-        gra.pieniadze+=    self.cena_calkowita
-        gra.lista_wiez.pop(gra.wybrana_wieza)
-    elif self.rodzaj==3:
-      if gra.polepszenie%4==0:
-        if gra.pieniadze>=40 and self.poziom_atak<=4:
-          self.cena_calkowita+= 40
-          gra.pieniadze-=       40
-          self.obrazenia+=      1
-          self.ilu_na_raz+=     1
-          self.poziom_atak+=    1
-      elif gra.polepszenie%4==1:
-        if gra.pieniadze>=4 and self.poziom_zasieg<=4:
-          self.cena_calkowita+= 4
-          gra.pieniadze-=       4
-          self.dlugosc_zycia+=  5
-          self.zasieg+=         15
-          self.poziom_zasieg+=  1
-      elif gra.polepszenie%4==2:
-        if gra.pieniadze>=30 and self.poziom_reszta<=4:
-          self.cena_calkowita+= 30
-          gra.pieniadze-=       30
-          self.predkosc+=       0.2
-          self.elektryzacja+=   1
-          self.poziom_reszta+=  1
-      elif gra.polepszenie%4==3:
-        gra.wybrano_wieze= False
-        gra.pieniadze+=    self.cena_calkowita
-        gra.lista_wiez.pop(gra.wybrana_wieza)
-    self.poziom= (self.poziom_atak+self.poziom_zasieg+self.poziom_reszta)//3
-    self.typ=    wieze[self.rodzaj-1+self.poziom*3][0]
+        elif self.rodzaj == 2:
+            self.koszt_atak = 10
+            self.koszt_zasieg = 2
+            self.koszt_reszta = 20
+            self.przebicie = 1
+
+        elif self.rodzaj == 3:
+            self.koszt_atak = 40
+            self.koszt_zasieg = 4
+            self.koszt_reszta = 30
+            self.ilu_na_raz = 3
+            self.elektryzacja = 0
+
+    def strzal(self, gra):
+        if self.licznik + self.przeladowanie <= gra.licznik:
+            self.licznik = gra.licznik
+            gra.lista_pociskow.append(Pocisk(self, gra))
+
+    def polepszenie(self, gra):
+        gra.wybrano_wieze = True
+
+        if self.rodzaj == 1:
+            if (gra.polepszenie % 4) == 0:
+                if gra.pieniadze >= 4 and self.poziom_atak <= 4:
+                    self.cena_calkowita += 4
+                    gra.pieniadze -= 4
+                    self.obrazenia += 5
+                    self.poziom_atak += 1
+
+            elif (gra.polepszenie % 4) == 1:
+                if gra.pieniadze >= 1 and self.poziom_zasieg <= 4:
+                    self.cena_calkowita += 1
+                    gra.pieniadze -= 1
+                    self.dlugosc_zycia += 30
+                    self.zasieg += 15
+                    self.poziom_zasieg += 1
+
+            elif (gra.polepszenie % 4) == 2:
+                if gra.pieniadze >= 2 and self.poziom_reszta <= 4:
+                    self.cena_calkowita += 2
+                    gra.pieniadze -= 2
+                    self.przeladowanie -= 1
+                    self.predkosc += 0.4
+                    self.poziom_reszta += 1
+
+            elif (gra.polepszenie % 4) == 3:
+                gra.wybrano_wieze = False
+                gra.pieniadze += self.cena_calkowita
+                gra.lista_wiez.pop(gra.wybrana_wieza)
+
+        elif self.rodzaj == 2:
+            if (gra.polepszenie % 4) == 0:
+                if gra.pieniadze >= 10 and self.poziom_atak <= 4:
+                    self.cena_calkowita += 10
+                    gra.pieniadze -= 10
+                    self.obrazenia += 20
+                    self.poziom_atak += 1
+
+            elif (gra.polepszenie % 4) == 1:
+                if gra.pieniadze >= 2 and self.poziom_zasieg <= 4:
+                    self.cena_calkowita += 2
+                    gra.pieniadze -= 2
+                    self.dlugosc_zycia += 15
+                    self.zasieg += 30
+                    self.poziom_zasieg += 1
+
+            elif (gra.polepszenie % 4) == 2:
+                if gra.pieniadze >= 20 and self.poziom_reszta <= 4:
+                    self.cena_calkowita += 20
+                    gra.pieniadze -= 20
+                    self.przeladowanie -= 1
+                    self.przebicie += 1
+                    self.poziom_reszta += 1
+
+            elif (gra.polepszenie % 4) == 3:
+                gra.wybrano_wieze = False
+                gra.pieniadze += self.cena_calkowita
+                gra.lista_wiez.pop(gra.wybrana_wieza)
+
+        elif self.rodzaj == 3:
+            if (gra.polepszenie % 4) == 0:
+                if gra.pieniadze >= 40 and self.poziom_atak <= 4:
+                    self.cena_calkowita += 40
+                    gra.pieniadze -= 40
+                    self.obrazenia += 1
+                    self.ilu_na_raz += 1
+                    self.poziom_atak += 1
+
+            elif (gra.polepszenie % 4) == 1:
+                if gra.pieniadze >= 4 and self.poziom_zasieg <= 4:
+                    self.cena_calkowita += 4
+                    gra.pieniadze -= 4
+                    self.dlugosc_zycia += 5
+                    self.zasieg += 15
+                    self.poziom_zasieg += 1
+
+            elif (gra.polepszenie % 4) == 2:
+                if gra.pieniadze >= 30 and self.poziom_reszta <= 4:
+                    self.cena_calkowita += 30
+                    gra.pieniadze -= 30
+                    self.predkosc += 0.2
+                    self.elektryzacja += 1
+                    self.poziom_reszta += 1
+
+            elif (gra.polepszenie % 4) == 3:
+                gra.wybrano_wieze = False
+                gra.pieniadze += self.cena_calkowita
+                gra.lista_wiez.pop(gra.wybrana_wieza)
+
+        self.poziom = (self.poziom_atak + self.poziom_zasieg + self.poziom_reszta) // 3
+        self.typ = wieze[self.rodzaj - 1 + (self.poziom * 3)][0]
+
 
 class Gra:
+
   def __init__(self):
     self.okno_gry= pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
     self.gracz=    Gracz()
